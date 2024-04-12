473fb25bfa454e4404eaa6cb3ebfb310fd8e7410
Shreya Gurram <gurram13775@gmail.com>
2024-04-12 23:43:05 +0530

    adds modal, nice cards and interactive bg

--- app/test/page.jsx
+++ app/test/page.jsx
@@ -2,6 +2,7 @@
 import { HoverEffect } from "../components/ui/car-hover-effect.tsx";
 import React, { useState } from "react";
 import Modal from "react-modal";
+import { BackgroundGradientAnimation } from "../components/ui/background-gradient-animation.tsx";
 
 export default function CardHoverEffectDemo() {
   const [modalOpen, setModalOpen] = useState(false);
@@ -19,45 +20,67 @@ export default function CardHoverEffectDemo() {
   ];
   const modalStyles = {
     overlay: {
-      zIndex: 1000,
+      zIndex: 100000,
     },
   };
+
   return (
     <div className="min-h-screen">
-      <div className="text-5xl lg:text-9xl text-center p-3">just do it,</div>
-      <div className="text-4xl text-center p-3">what are you waiting for?</div>
-      <div className="max-w-6xl mx-auto px-8 z-10">
-        <HoverEffect items={projects} />
-      </div>
-      <div className="max-w-6xl mx-auto px-8">
-        {plus.map((item, index) => (
-          <div key={index} onClick={toggleModal}>
-            {item.title}
-          </div>
-        ))}
-      </div>
+      <BackgroundGradientAnimation>
+        <div className="text-5xl lg:text-9xl mt-2 text-center p-3">
+          just do it,
+        </div>
+        <div className="text-4xl text-center p-3">
+          what are you waiting for?
+        </div>
+        <div className="max-w-6xl mx-auto px-8 z-10">
+          <HoverEffect items={projects} />
+        </div>
+        <div className="max-w-6xl mx-auto px-8 z-10">
+          {plus.map((item, index) => (
+            <div key={index} onClick={toggleModal}>
+              <HoverEffect items={plus} />
+            </div>
+          ))}
+        </div>
+      </BackgroundGradientAnimation>
       <Modal
         style={modalStyles}
         isOpen={modalOpen}
         onRequestClose={toggleModal}
         contentLabel="Add New To-Do Item"
+        className="min-h-screen m-8 backdrop-blur-sm bg-black/50 "
       >
-        <div className="modal-content z-0">
-          <div className="text-4xl flex justify-center items-center p-4 m-3">
+        <div className="modal-content z-0 ">
+          <div className="text-4xl flex justify-center items-center p-4 m-3 font-bold">
             Add New To-Do Item
           </div>
           <div className="grid grid-rows-3 gap-4 justify-items-center">
-            <input type="text" placeholder=" Title" className="rounded-md" />
             <input
               type="text"
-              placeholder="Description"
-              className="rounded-md"
+              className=" bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block  p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              placeholder="Title"
             />
-            <input type="text" placeholder="Link" className="rounded-md" />
+            <input
+              type="text"
+              className=" bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block  p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              placeholder="Description"
+            />
+            <input
+              type="text"
+              className=" bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block  p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              placeholder="Link"
+            />
           </div>
-          <div className="text-2xl flex justify-center items-center p-4 m-3">
-            <button onClick={toggleModal}>Close</button>
-          </div>
+        </div>
+        <div className="text-2xl flex justify-center items-center p-4 m-3">
+          <button
+            type="button"
+            onClick={toggleModal}
+            className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"
+          >
+            Close
+          </button>
         </div>
       </Modal>
     </div>
