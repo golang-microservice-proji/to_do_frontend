b9878b2012aa10a6ed0e9598689f49e0d151ed07
Shreya Gurram <gurram13775@gmail.com>
2024-04-12 21:28:29 +0530

    basic commit

---
+++ app/test/page.jsx
@@ -0,0 +1,34 @@
+"use client";
+import { HoverEffect } from "../components/ui/car-hover-effect.tsx";
+
+export default function CardHoverEffectDemo() {
+  return (
+    <div className="min-h-screen">
+      <div className=" text-9xl"> IMMA CRY</div>
+      <div className="max-w-6xl mx-auto px-8">
+        <HoverEffect items={projects} />
+      </div>
+    </div>
+  );
+}
+export const projects = [
+  // fetch from db
+  {
+    title: "Stripe",
+    description:
+      "A technology company that builds economic infrastructure for the internet.",
+    link: "https://stripe.com",
+  },
+  {
+    title: "Netflix",
+    description:
+      "A streaming service that offers a wide variety of award-winning TV shows, movies, anime, documentaries, and more on thousands of internet-connected devices.",
+    link: "https://netflix.com",
+  },
+  {
+    title: "Google",
+    description:
+      "A multinational technology company that specializes in Internet-related services and products.",
+    link: "https://google.com",
+  },
+];
